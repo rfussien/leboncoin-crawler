d2eea215c6fcb86d580bd905bd37194fe5278bef
Remi FUSSIEN <remi.fussien@elbee.fr>
2017-04-07 14:20:39 +0200

    Fix StyleCi fail

--- src/Filter/PrixSanitizer.php
+++ src/Filter/PrixSanitizer.php
@@ -4,7 +4,6 @@ namespace Lbc\Filter;
 
 class PrixSanitizer implements SanitizerInterface
 {
-
     public static function clean($value)
     {
         return (int) preg_replace('/\D/', '', trim($value));
--- src/Filter/SurfaceSanitizer.php
+++ src/Filter/SurfaceSanitizer.php
@@ -4,7 +4,6 @@ namespace Lbc\Filter;
 
 class SurfaceSanitizer implements SanitizerInterface
 {
-
     public static function clean($value)
     {
         return (int) preg_replace('/ cm2/', '', DefaultSanitizer::clean($value));
